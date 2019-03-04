405bbf28166f6268c6dba2f81ca25fe999cea5cf
Muqriz <muqrizx@gmail.com>
2019-03-04 08:54:07 +0800

    added Myconnection.php
    edit if nessacary

---
+++ Myconnection.php
@@ -0,0 +1,12 @@
+<?php
+
+//Declaring connection to the localhost server
+$con = mysqli_connect("localhost","root","","educreate");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+?>
